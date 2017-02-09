b697227dcc213a6901775843f4d24a79fc2902f8
Michael Ries <michael@riesd.com>
2017-02-09 10:54:49 -0700

    got a basic drawing of a screen working

--- lib/extank.ex
+++ lib/extank.ex
@@ -1,18 +1,60 @@
 defmodule Extank do
-  @moduledoc """
-  Documentation for Extank.
-  """
+  @behaviour :wx_object
 
-  @doc """
-  Hello world.
+  @title 'Elixir OpenGL'
+  @size {600, 600}
 
-  ## Examples
+  def start(_type, _args) do
+    {_, _, _, pid} = :wx_object.start_link(__MODULE__, [], [name: :extank])
+    {:ok, pid}
+  end
 
-      iex> Extank.hello
-      :world
+  def init(config) do
+    title = 'ExTanks'
+    size = {600, 600}
+    wx = :wx.new(config)
+    frame = :wxFrame.new(wx, :wx_const.wxID_ANY(), title, [{:size, size}])
+    true = :wxWindow.setBackgroundColour(frame, :wx_const.wxBLACK())
+    :wxWindow.connect(frame, :close_window)
+    :wxWindow.connect(frame, :size)
+    :wxFrame.show(frame)
 
-  """
-  def hello do
-    :world
+    # once we know how many tanks there are load images for them
+    bitmap = :wxBitmap.new()
+    true = :wxBitmap.loadFile(bitmap, "blue_body000.jpg", [type: :wx_const.wxBITMAP_TYPE_JPEG()])
+
+    timer = :timer.send_interval(20, self(), :re_draw)
+
+    {frame, %{frame: frame, bitmap: bitmap, timer: timer}}
+  end
+
+  def handle_event({:wx, _, _, _, {:wxClose, :close_window}}, state) do
+    {:stop, :normal, state}
+  end
+
+  def handle_event({:wx, _, _, _, {:wxSize, :size, {width, height}, _}}, state) do
+    IO.puts "RESIZE: #{width}x#{height}"
+    draw(state.frame, state.bitmap)
+    {:noreply, state}
+  end
+
+  def handle_info(:re_draw, state) do
+    draw(state.frame, state.bitmap)
+    {:noreply, state}
+  end
+
+  def terminate(_, %{bitmap: bitmap, timer: timer}) do
+    :wxBitmap.destroy(bitmap)
+    :timer.cancel(timer)
+  end
+
+  defp draw(frame, bitmap) do
+    IO.puts "start DRAW"
+    graphics_context = :wxGraphicsContext.create(frame)
+    #:wxWindow.clearBackground(frame)
+    :wxGraphicsContext.drawBitmap(graphics_context, bitmap, 200, 200, 36, 38)
+    # draw other stuff
+    :wxGraphicsContext.destroy(graphics_context)
+    IO.puts "finish DRAW"
   end
 end
--- mix.exs
+++ mix.exs
@@ -10,24 +10,16 @@ defmodule Extank.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [
+      extra_applications: [:logger],
+      mod: {Extank, []},
+    ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:wx_utils, "~> 0.0.2"},
+    ]
   end
 end
